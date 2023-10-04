fbc81e10ccbc6e977447ddd7aac85c6a5b9e3941
d-l-walker <danielwalker274@gmail.com>
2023-10-04 17:45:07 +0100

    Add exceptions for corrupted downloads

--- make_moments_and_movies.py
+++ make_moments_and_movies.py
@@ -36,59 +36,67 @@ for index, row in df.iterrows():
         start, end = [int(x) for x in row['chans'].split('~')]
 
         # Get the start and end frequencies from the fits file based on the channel range
-        ia.open(file)
-        cs = ia.coordsys()
-        start_freq = ((cs.toworld([0,0,start,0])['numeric'][2])*u.Hz).to(u.GHz)
-        end_freq = ((cs.toworld([0,0,end,0])['numeric'][2])*u.Hz).to(u.GHz)
-        ia.close()
-
-        ext = '.moment0.integrated_{}-{}GHz'.format(np.around(start_freq.value, decimals=3), np.around(end_freq.value, decimals=3))
+        file_ok = True
+        try:
+            ia.open(file)
+            cs = ia.coordsys()
+            start_freq = ((cs.toworld([0,0,start,0])['numeric'][2])*u.Hz).to(u.GHz)
+            end_freq = ((cs.toworld([0,0,end,0])['numeric'][2])*u.Hz).to(u.GHz)
+            ia.close()
+        # Sometimes downloads fail and the fits file is corrupted. If this happens, skip the file and log the error
+        except Exception as e:
+            logging.error('Error: %s', e)
+            logging.error('Error: %s', file)
+            file_ok = False
         
-        if make_moments:
-            outname_moments = file.replace('.fits', '.'+mol+ext)
-            if not os.path.exists('./'+row['project']+'/'+outname_moments+'.fits'):
-                immoments(imagename=file,
-                        moments=[0],
-                        axis='spectral',
-                        chans=str(start)+'~'+str(end),
-                        outfile='./'+row['project']+'/'+outname_moments)
+        if file_ok:
+            ext = '.moment0.integrated_{}-{}GHz'.format(np.around(start_freq.value, decimals=3), np.around(end_freq.value, decimals=3))
+            
+            if make_moments:
+                outname_moments = file.replace('.fits', '.'+mol+ext)
+                if not os.path.exists('./'+row['project']+'/'+outname_moments+'.fits'):
+                    immoments(imagename=file,
+                            moments=[0],
+                            axis='spectral',
+                            chans=str(start)+'~'+str(end),
+                            outfile='./'+row['project']+'/'+outname_moments)
 
-                exportfits(imagename='./'+row['project']+'/'+outname_moments, fitsimage='./'+row['project']+'/'+outname_moments+'.fits', overwrite=True)
-                
-                try:
-                    path = os.path.join(row['project'], outname_moments)
-                    shutil.rmtree(path)
-                except OSError as e:
-                    logging.error(f"Error: {e.filename} - {e.strerror}.")
-            else:
-                logging.info('Moment map already exists: %s', outname_moments)
+                    exportfits(imagename='./'+row['project']+'/'+outname_moments, fitsimage='./'+row['project']+'/'+outname_moments+'.fits', overwrite=True)
+                    
+                    try:
+                        path = os.path.join(row['project'], outname_moments)
+                        shutil.rmtree(path)
+                    except OSError as e:
+                        logging.error(f"Error: {e.filename} - {e.strerror}.")
+                else:
+                    logging.info('Moment map already exists: %s', outname_moments)
 
-        if make_movies:
-            outname_movie = file.replace('.fits', '.'+mol+ext+'.mp4')
-            if not os.path.exists('./'+row['project']+'/'+outname_movie):
-                importfits(fitsimage=file, imagename=file.replace('.fits', '.image'), overwrite=True)
-                if os.path.isdir('./temp'):
-                    shutil.rmtree('./temp')
+            if make_movies:
+                outname_movie = file.replace('.fits', '.'+mol+ext+'.mp4')
+                if not os.path.exists('./'+row['project']+'/'+outname_movie):
+                    importfits(fitsimage=file, imagename=file.replace('.fits', '.image'), overwrite=True)
+                    if os.path.isdir('./temp'):
+                        shutil.rmtree('./temp')
 
-                os.makedirs('temp', exist_ok=True)
+                    os.makedirs('temp', exist_ok=True)
 
-                for chan in range(start, end):
-                    imsubimage(imagename=file.replace('.fits', '.image'), outfile='./temp/'+str(chan)+'.image', chans=str(chan), overwrite=False)
+                    for chan in range(start, end):
+                        imsubimage(imagename=file.replace('.fits', '.image'), outfile='./temp/'+str(chan)+'.image', chans=str(chan), overwrite=False)
 
-                for chan in range(start, end):
-                    chan_image = "./temp/" + str(chan) + ".image"
-                    chan_png = "./temp/" + str(chan) + ".png"
-                    imview(raster=chan_image, out=chan_png)
+                    for chan in range(start, end):
+                        chan_image = "./temp/" + str(chan) + ".image"
+                        chan_png = "./temp/" + str(chan) + ".png"
+                        imview(raster=chan_image, out=chan_png)
 
-                os.system('ffmpeg -framerate 10 -start_number ' + str(start) + ' -i ./temp/%d.png ' + './'+row['project']+'/'+outname_movie)
-            else:
-                logging.info('Movie already exists: %s', outname_movie)
+                    os.system('ffmpeg -framerate 10 -start_number ' + str(start) + ' -i ./temp/%d.png ' + './'+row['project']+'/'+outname_movie)
+                else:
+                    logging.info('Movie already exists: %s', outname_movie)
 
-        if os.path.isdir('./temp'):
-            logging.info('Cleaning up temp directory')
-            shutil.rmtree('./temp')
-        if file:
-            file_to_remove = file.replace('.fits', '.image')
-            if os.path.isdir(file_to_remove):
-                logging.info('Cleaning up image files')
-                shutil.rmtree(file_to_remove)
+            if os.path.isdir('./temp'):
+                logging.info('Cleaning up temp directory')
+                shutil.rmtree('./temp')
+            if file:
+                file_to_remove = file.replace('.fits', '.image')
+                if os.path.isdir(file_to_remove):
+                    logging.info('Cleaning up image files')
+                    shutil.rmtree(file_to_remove)
